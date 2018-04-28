b3fe3479c204985e62b4e8073cd3b66db15fda4c
HardikSuthar3 <hsuthar326@gmail.com>
2018-04-28 11:02:16 +0530

    Change from Mac CLion

--- cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CompetitiveProgramming.dir/Main.cpp.o"
-  "CompetitiveProgramming.pdb"
-  "CompetitiveProgramming"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/CompetitiveProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
