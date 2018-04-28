7fccec1b3de8a19b876614d0934024c5483139cc
HardikSuthar3 <hsuthar326@gmail.com>
2018-04-28 11:21:13 +0530

    Changes from VS Code Mac

--- cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
@@ -1,5 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompetitiveProgramming.dir/Main.cpp.o"
+  "CompetitiveProgramming.pdb"
+  "CompetitiveProgramming"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/CompetitiveProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
