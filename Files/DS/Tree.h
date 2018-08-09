ae549366d95dae5bd2d73b572f183cf47016f999
Hardik Suthar <hasuthar@microsoft.com>
2018-08-09 23:33:57 +0530

    change from win laptop

--- Files/DS/Tree.h
+++ Files/DS/Tree.h
@@ -7,216 +7,216 @@ using namespace std;
 
 class Node
 {
-  public:
-    int data;
-    Node *left, *right;
-    Node(int data)
-    {
-        this->data = data;
-        this->left = NULL;
-        this->right = NULL;
-        //nodeList.insert(make_pair(data, this));
-    }
+public:
+	int data;
+	Node *left, *right;
+	Node(int data)
+	{
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+		//nodeList.insert(make_pair(data, this));
+	}
 };
 
 class BinaryTree
 {
-  public:
-    Node *root;
-    map<int, Node *> nodeList;
-    Node *addNode(int s, int l, int r);
-    void Make_Tree(int N);
-    bool Set_root(int r);
-    void inOrderTraversal(Node *start);
-    void preOrderTraversal(Node *start);
-    void postOrderTraversal(Node *start);
-    void BFS(Node *start);
-    void DFS(Node *start);
+public:
+	Node * root;
+	map<int, Node *> nodeList;
+	Node *addNode(int s, int l, int r);
+	void Make_Tree(int N);
+	bool Set_root(int r);
+	void inOrderTraversal(Node *start);
+	void preOrderTraversal(Node *start);
+	void postOrderTraversal(Node *start);
+	void BFS(Node *start);
+	void DFS(Node *start);
 };
 
 Node *BinaryTree::addNode(int s, int l, int r)
 {
-    if (s < 0)
-    {
-        return NULL;
-    }
+	if (s < 0)
+	{
+		return NULL;
+	}
 
-    Node *S;
+	Node *S;
 
-    if (nodeList.find(s) == nodeList.end())
-    {
-        S = new Node(s);
-        nodeList.insert(make_pair(s, S));
-    }
-    else
-    {
-        S = nodeList[s];
-    }
+	if (nodeList.find(s) == nodeList.end())
+	{
+		S = new Node(s);
+		nodeList.insert(make_pair(s, S));
+	}
+	else
+	{
+		S = nodeList[s];
+	}
 
-    if (l > 0)
-    {
-        S->left = (nodeList.find(l) != nodeList.end()) ? nodeList[l] : addNode(l, -1, -1);
-    }
-    else
-    {
-        if (S->left != NULL)
-            ;
-        else
-        {
-            S->left = NULL;
-        }
-    }
+	if (l > 0)
+	{
+		S->left = (nodeList.find(l) != nodeList.end()) ? nodeList[l] : addNode(l, -1, -1);
+	}
+	else
+	{
+		if (S->left != NULL)
+			;
+		else
+		{
+			S->left = NULL;
+		}
+	}
 
-    if (r > 0)
-    {
-        S->right = (nodeList.find(r) != nodeList.end()) ? nodeList[r] : addNode(r, -1, -1);
-    }
-    else
-    {
-        if (S->right != NULL)
-            ;
-        else
-        {
-            S->right = NULL;
-        }
-    }
-    return S;
+	if (r > 0)
+	{
+		S->right = (nodeList.find(r) != nodeList.end()) ? nodeList[r] : addNode(r, -1, -1);
+	}
+	else
+	{
+		if (S->right != NULL)
+			;
+		else
+		{
+			S->right = NULL;
+		}
+	}
+	return S;
 }
 
 void BinaryTree::Make_Tree(int N)
 {
-    for (int i = 0; i < N - 1; i++)
-    {
-        int par, child;
-        char ch;
-        cin >> par >> child >> ch;
-        switch (ch)
-        {
-        case 'L':
-            addNode(par, child, -1);
-            break;
-        default:
-            addNode(par, -1, child);
-            break;
-        }
-    }
+	for (int i = 0; i < N - 1; i++)
+	{
+		int par, child;
+		char ch;
+		cin >> par >> child >> ch;
+		switch (ch)
+		{
+		case 'L':
+			addNode(par, child, -1);
+			break;
+		default:
+			addNode(par, -1, child);
+			break;
+		}
+	}
 }
 
 bool BinaryTree::Set_root(int r)
 {
-    if (nodeList.find(r) == nodeList.end())
-    {
-        return false;
-    }
+	if (nodeList.find(r) == nodeList.end())
+	{
+		return false;
+	}
 
-    this->root = nodeList.at(r);
-    return true;
+	this->root = nodeList.at(r);
+	return true;
 }
 
 void BinaryTree::inOrderTraversal(Node *start)
 {
-    if (start->left != NULL)
-    {
-        inOrderTraversal(start->left);
-    }
-    cout << start->data << endl;
+	if (start->left != NULL)
+	{
+		inOrderTraversal(start->left);
+	}
+	cout << start->data << endl;
 
-    if (start->right != NULL)
-    {
-        inOrderTraversal(start->right);
-    }
+	if (start->right != NULL)
+	{
+		inOrderTraversal(start->right);
+	}
 }
 
 void BinaryTree::preOrderTraversal(Node *start)
 {
-    cout << start->data << endl;
+	cout << start->data << endl;
 
-    if (start->left != NULL)
-    {
-        inOrderTraversal(start->left);
-    }
+	if (start->left != NULL)
+	{
+		inOrderTraversal(start->left);
+	}
 
-    if (start->right != NULL)
-    {
-        inOrderTraversal(start->right);
-    }
+	if (start->right != NULL)
+	{
+		inOrderTraversal(start->right);
+	}
 }
 
 void BinaryTree::postOrderTraversal(Node *start)
 {
-    cout << start->data << endl;
+	cout << start->data << endl;
 
-    if (start->left != NULL)
-    {
-        inOrderTraversal(start->left);
-    }
+	if (start->left != NULL)
+	{
+		inOrderTraversal(start->left);
+	}
 
-    if (start->right != NULL)
-    {
-        inOrderTraversal(start->right);
-    }
+	if (start->right != NULL)
+	{
+		inOrderTraversal(start->right);
+	}
 }
 
 void BinaryTree::BFS(Node *start)
 {
-    if (start == NULL)
-    {
-        return;
-    }
+	if (start == NULL)
+	{
+		return;
+	}
 
-    Node *u = start;
+	Node *u = start;
 
-    set<int> visited;
+	set<int> visited;
 
-    queue<Node *> que;
-    que.push(u);
+	queue<Node *> que;
+	que.push(u);
 
-    while (!que.empty())
-    {
-        Node *node = que.front();
-        que.pop();
-        visited.insert(node->data);
+	while (!que.empty())
+	{
+		Node *node = que.front();
+		que.pop();
+		visited.insert(node->data);
 
-        cout << node->data << " ";
-        if (node->left != NULL && binary_search(visited.begin(), visited.end(), node->left->data) == false)
-        {
-            que.push(node->left);
-        }
+		cout << node->data << " ";
+		if (node->left != NULL && binary_search(visited.begin(), visited.end(), node->left->data) == false)
+		{
+			que.push(node->left);
+		}
 
-        if (node->right != NULL && binary_search(visited.begin(), visited.end(), node->right->data) == false)
-        {
-            que.push(node->right);
-        }
-    }
+		if (node->right != NULL && binary_search(visited.begin(), visited.end(), node->right->data) == false)
+		{
+			que.push(node->right);
+		}
+	}
 }
 
 void BinaryTree::DFS(Node *start)
 {
-    static set<int> visited;
+	static set<int> visited;
 
-    if (start == NULL)
-    {
-        return;
-    }
+	if (start == NULL)
+	{
+		return;
+	}
 
-    if (binary_search(visited.begin(), visited.end(), start->data))
-    {
-        return;
-    }
+	if (binary_search(visited.begin(), visited.end(), start->data))
+	{
+		return;
+	}
 
-    int x = start->data;
-    visited.insert(x);
-    cout << x << " ";
+	int x = start->data;
+	visited.insert(x);
+	cout << x << " ";
 
-    auto tmp = start;
+	auto tmp = start;
 
-    if (tmp->left != NULL)
-    {
-        DFS(tmp->left);
-    }
+	if (tmp->left != NULL)
+	{
+		DFS(tmp->left);
+	}
 
-    if (tmp->right != NULL)
-    {
-        DFS(tmp->right);
-    }
+	if (tmp->right != NULL)
+	{
+		DFS(tmp->right);
+	}
 }
